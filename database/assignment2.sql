acf6681841413cdc5ebcc67c41048f044f85f00d
DjPatbern <victorbernardpatrick@gmail.com>
2025-09-13 15:47:01 +0100

    database creation

---
+++ database/assignment2.sql
@@ -0,0 +1,38 @@
+-- 1️ Insert Tony Stark into account table
+INSERT INTO public.account (
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n'); 
+
+-- 2️ Modify Tony Stark's account_type to Admin using account_id
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_id = (
+    SELECT account_id FROM public.account WHERE account_email = 'tony@starkent.com'
+);
+
+-- 3️ Delete Tony Stark's record using account_id
+DELETE FROM public.account
+WHERE account_id = (
+    SELECT account_id FROM public.account WHERE account_email = 'tony@starkent.com'
+);
+
+-- 4️ Modify GM Hummer description to replace "small interiors" with "a huge interior"
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 5️ Inner join to get make, model, and classification_name for Sport vehicles
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM public.inventory i
+INNER JOIN public.classification c
+  ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+-- 6️ Update all inventory image paths to include "/vehicles"
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
